36d0003ae5738ebeb414287fb7fb0664455e4b93
asoleymani <asoleymani@gmail.com>
2022-02-19 23:11:11 +0330

    form

--- src/App.js
+++ src/App.js
@@ -2,28 +2,35 @@ import './App.css';
 import { useForm } from 'react-hook-form';
 
 function App() {
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = (data) => {
-    console.log(data);
+  const defaultValues = {
+    firstName: 'bifefefefell',
+    lastName: 'luoffffdddd',
+  };
+
+  const {
+    register, handleSubmit, formState: { errors },
+  } = useForm();
+  const onSubmit = (data, e) => {
+    e.target.reset(); // reset after form submit
+    alert(JSON.stringify(data));
   };
   return (
     <div className="App">
       <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="firstname">
           firstName:
-          {' '}
-          <input id="firstname" {...register('firstName')} />
+          <input defaultValue={defaultValues.firstName} id="firstname" {...register('firstName')} />
         </label>
         <label htmlFor="lastname">
           lastname:
-          <input id="lastname" {...register('lastName', { required: true })} />
+          <input defaultValue={defaultValues.lastName} id="lastname" {...register('lastName', { required: 'this is required', minLength: { value: 10, message: 'must 10' } })} />
         </label>
-        {errors.lastName && <p>Last name is required.</p>}
+        {errors.lastName && errors.lastName.message}
         <label htmlFor="age">
           age:
-          <input id="age" {...register('age', { pattern: /\d+/, type: Number })} />
+          <input id="age" {...register('age', { required: 'this is required', pattern: { value: /\d+/, message: 'pattren' } })} />
         </label>
-        {errors.age && <p>Please enter number for age.</p>}
+        {errors.age && errors.age.message}
         <input type="submit" />
       </form>
     </div>
